d20bd66334e3e2d19646d998e906b22d554be335
Ronak Sharma <rsvk1819@gmail.com>
2025-09-19 21:51:00 +0530

    [Test] : Added test for missing fields in user registration

--- backend/__test__/user_register.test.js
+++ backend/__test__/user_register.test.js
@@ -1,13 +1,36 @@
 const request = require("supertest");
 const app = require("../src/index");
 
-describe("Auth API", () => {
+describe("Auth Registration API", () => {
   it("should register a new user", async () => {
-    const res = await request(app)
-      .post("/api/auth/register")
-      .send({ email: "test@example.com", password: "123456" ,name :"Test User" });
+    const res = await request(app).post("/api/auth/register").send({
+      email: "test19@example.com",
+      password: "123456",
+      name: "Test User",
+    });
 
     expect(res.statusCode).toBe(201); // Check if the status code is 201
     expect(res.body).toHaveProperty("token"); // Check if the response body has a token property
   });
+  it("should return 400 if name is missing", async () => {
+    const res = await request(app).post("/api/auth/register").send({
+      email: "test@example.com",
+      password: "123456",
+    });
+    expect(res.statusCode).toBe(400);
+    expect(res.body).toHaveProperty(
+      "error",
+      "Name, email and password are required"
+    );
+  });
+
+  it("should return 400 if email is missing", async () => {
+    const res = await request(app).post("/api/auth/register").send({
+      name: "Test User",
+      password: "123456",
+    });
+    expect(res.statusCode).toBe(400);
+  });
+
+  
 });
