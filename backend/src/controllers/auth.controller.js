945ec91b5712963b311ee6dc185a9ad314274a83
Ronak Sharma <rsvk1819@gmail.com>
2025-09-19 22:00:00 +0530

    [Feat] : Added validation for duplicate email prevention

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,20 +1,25 @@
-const User=require('../models/user.model')
+const User = require("../models/user.model");
 const jwt = require("jsonwebtoken");
 
 const generateToken = (id) => {
-  return jwt.sign({ id }, process.env.JWT_SECRET || "secret", { expiresIn: "1h" });
+  return jwt.sign({ id }, process.env.JWT_SECRET || "secret", {
+    expiresIn: "1h",
+  });
 };
-const registerUser =async (req, res) => {
+const registerUser = async (req, res) => {
   const { name, email, password } = req.body;
 
-
-    if (!name || !email || !password) {
-    return res.status(400).json({ error: "Name, email and password are required" });
+  if (!name || !email || !password) {
+    return res
+      .status(400)
+      .json({ error: "Name, email and password are required" });
   }
   try {
     const existingUser = await User.findOne({ email });
     // check if user already exists
-    if (existingUser) return res.status(400).json({ error: "User already exists" }); 
+    if (existingUser) {
+      return res.status(400).json({ error: "User already exists" });
+    }
     // create new user
     const user = await User.create({ name, email, password });
 
