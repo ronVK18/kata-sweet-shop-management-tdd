4956bcdb9998de65449a980d642dd2df4837d892
Ronak Sharma <rsvk1819@gmail.com>
2025-09-20 16:41:15 +0530

    [Feat] : Added a role field in user model

--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -6,6 +6,11 @@ const userSchema = new mongoose.Schema(
     name: { type: String, required: true, trim: true },
     email: { type: String, required: true, unique: true, lowercase: true },
     password: { type: String, required: true },
+    role: {
+      type: String,
+      enum: ["user", "admin"],
+      default: "user",
+    },
   },
   { timestamps: true }
 );
